87f4b2def3bd5b503137253c8a276928f780ca87
Parajulibkrm <bikramparajuli0000@gmail.com>
2023-05-05 13:21:47 +0545

    updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import TweetBody from "./components/TweetBody";
-
+import "./App.css";
 const App = () => {
   return (
     <>
       <div className="tweet">
         <img
-          className="profile-pic"
           src="https://placekitten.com/50/50"
           alt="Profile Picture"
+          height={50}
+          width={50}
         />
         <div className="tweet-content">
           <Header />
--- src/components/TweetBody.jsx
+++ src/components/TweetBody.jsx
@@ -3,9 +3,10 @@ const TweetBody = () => {
     <>
       <p className="tweet-text">This is a sample tweet!</p>
       <img
-        className="tweet-image"
         src="https://placekitten.com/200/150"
         alt="Tweet Image"
+        height={300}
+        width={300}
       />
     </>
   );
